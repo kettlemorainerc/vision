b9244768badd1017d23b2cef13ff05170e0f105f
SquEdgy <dafman6@outlook.com>
2023-01-07 13:37:36 -0600

    Add basic opencv cpp program

---
+++ cpp/src/main.cpp
@@ -0,0 +1,16 @@
+#include "./main.hpp"
+#include <opencv2/opencv.hpp>
+
+int main(int argCount, char* argv[]) {
+    cv::Mat image;
+    image = cv::imread( argv[1], 1 );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
+}
